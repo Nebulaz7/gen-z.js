cb3972e0ddd72613ce74a4b773f6c0d301cddc88
Nebulaz7 <jp0712002@gmail.com>
2025-08-28 16:10:07 +0100

    feat: Added a contibute section

---
+++ web/genz-web/app/components/Contribute.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { GitBranch, ExternalLink } from "lucide-react";
+
+const Contribute = () => {
+  return (
+    // <div>
+    //   <h1>
+    //
+    //   </h1>
+
+    //   <p>
+    //
+    //   </p>
+    // </div>
+    <section className="relative w-[80vw] mx-auto my-5 rounded-4xl bg-gradient-to-b shadow-[4px_4px_0px_0px_#fafa10] from-gray-100 to-gray-200">
+      <div className="relative flex flex-col items-center justify-center px-6 py-10">
+        <div className="max-w-4xl mx-auto text-center">
+          {/* Main heading */}
+          <h2 className="hero-font text-3xl md:text-4xl lg:text-6xl font-light leading-tight text-black mb-6">
+            <span>
+              {" "}
+              <GitBranch size={24} />
+            </span>{" "}
+            Want to contribute?
+          </h2>
+
+          {/* Description */}
+          <p className="text-lg md:text-xl text-black pb-4 max-w-3xl mx-auto leading-relaxed">
+            Join our community and help make this project better, contribute
+            code, documentation, or ideas to make to help this project grow.
+          </p>
+          <button className="text-[#f2f2f2] cursor-pointer bg-[#000000] px-4 py-2 ml-3 mt-2 rounded-full shadow-[2px_2px_0px_0px_#fafa10] hover:shadow-[1px_1px_0px_0px_#fafa10] hover:scale-90 transition duration-300">
+            Open source
+            <ExternalLink className="inline-block ml-1 mb-1" size={22} />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contribute;
--- web/genz-web/app/page.tsx
+++ web/genz-web/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "./components/Features";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Contribute from "./components/Contribute";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Features />
+      <Contribute />
     </div>
   );
 }
