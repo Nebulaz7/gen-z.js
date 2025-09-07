ed827d180a9f51285d00191d7af0865cccc7e417
Nebulaz7 <jp0712002@gmail.com>
2025-09-07 02:25:23 +0100

    feat: add logo page component with image rendering

---
+++ web/app/app/logo/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const page = () => {
+  return <Image src="/logo.svg" alt="logo" width={200} height={200} />;
+};
+
+export default page;
